21d27a40159379eb9c176a72b70ea4b874ef2dea
Andressa Reis Araujo <andressareis98@outlook.com>
2023-10-01 11:48:56 -0300

    feat: adicionando o arquivo de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
+
 import { Header } from "./components/Header";
-import { Profile } from "./pages/Profile";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-      <Header />
-      <Profile />
+      <BrowserRouter>
+        <GlobalStyle />
+        <Header />
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
